6f05d66cf0ecb2a17b606df5f219921be8d73d75
AJHosuja <t1vian03@students.oamk.fi>
2022-06-16 11:47:57 +0300

    authethicate done

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/login', login);
 
-//app.use(authenticateToken);
+app.use(authenticateToken);
 
 app.use('/', indexRouter);
 app.use('/product', addProductRouter);
