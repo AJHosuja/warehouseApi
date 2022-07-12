a36ca0a4fa51784ccbea799e7e88f8ef0e60d804
AJHosuja <t1vian03@students.oamk.fi>
2022-07-12 14:06:06 +0300

    database updated

--- database.js
+++ database.js
@@ -1,24 +1,23 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: 'remotemysql.com',
-    user: '6S5GIfvfoo',
-    password: 'trK1DsBp8f',
-    database: '6S5GIfvfoo'
+  host: "sql11.freemysqlhosting.net",
+  user: "sql11505843",
+  password: "sFIbcsaTn6",
+  database: "sql11505843",
 });
 
-connection.connect(function(error){
-    if(!!error){
-        console.log(error);
-    } else{
-        console.log('connected...!');
-
-    }
+connection.connect(function (error) {
+  if (!!error) {
+    console.log(error);
+  } else {
+    console.log("connected...!");
+  }
 });
 
 setInterval(function () {
-    connection.query('SELECT 1');
-    console.log('query')
+  connection.query("SELECT 1");
+  console.log("query");
 }, 5000);
 
 module.exports = connection;
