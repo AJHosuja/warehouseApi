e36ef9f50c7b7fad5db5e9175f7dbafdaea9f6b4
AJHosuja <t1vian03@students.oamk.fi>
2022-08-24 01:58:56 +0300

    Fixing a bug

--- routes/testToken.js
+++ routes/testToken.js
@@ -7,25 +7,29 @@ const loginCRUD = require("../models/login");
 
 router.get("/:user", (req, response) => {
   const userName = req.params.user;
+  console.log(userName);
 
-  loginCRUD.getPassWord(req.body.user, (dberr, dbres) => {
+  loginCRUD.getPassWord(userName, (dberr, dbres) => {
     if (dberr) {
       response.send(false);
     } else {
-      let admin = false;
-      if (dbres[0].admin) {
-        console.log("here");
-        admin = true;
+      if (dbres[0]?.username) {
+        let admin = false;
+        if (dbres[0]?.admin) {
+          console.log("here");
+          admin = true;
+        }
+        const token = generateAccessToken({ username: userName });
+        var resultJSON = {
+          success: true,
+          token: token,
+          user: userName,
+          admin: admin,
+        };
+        response.send(resultJSON);
+      } else {
+        response.send(false);
       }
-
-      const token = generateAccessToken({ username: userName });
-      var resultJSON = {
-        success: true,
-        token: token,
-        user: userName,
-        admin: admin,
-      };
-      response.send(resultJSON);
     }
   });
 });
