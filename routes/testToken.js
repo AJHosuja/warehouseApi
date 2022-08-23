013d8bb9249f99ffa1c5c32017ea84640286fc17
AJHosuja <t1vian03@students.oamk.fi>
2022-08-24 01:35:02 +0300

    token validation update

--- routes/testToken.js
+++ routes/testToken.js
@@ -3,17 +3,31 @@ const { response } = require("../app");
 var router = express.Router();
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
+const loginCRUD = require("../models/login");
 
 router.get("/:user", (req, response) => {
   const userName = req.params.user;
-  const token = generateAccessToken({ username: userName });
-  var resultJSON = {
-    success: true,
-    token: token,
-    user: userName,
-  };
 
-  response.send(resultJSON);
+  loginCRUD.getPassWord(req.body.user, (dberr, dbres) => {
+    if (dberr) {
+      response.send(false);
+    } else {
+      let admin = false;
+      if (dbres[0].admin) {
+        console.log("here");
+        admin = true;
+      }
+
+      const token = generateAccessToken({ username: userName });
+      var resultJSON = {
+        success: true,
+        token: token,
+        user: userName,
+        admin: admin,
+      };
+      response.send(resultJSON);
+    }
+  });
 });
 
 function generateAccessToken(username) {
