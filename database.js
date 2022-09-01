4e370989de99ac15aa3db1070eb845e2d91e3449
AJHosuja <t1vian03@students.oamk.fi>
2022-09-01 22:29:56 +0300

    Database changed

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "sql11.freemysqlhosting.net",
-  user: "sql11505843",
-  password: "sFIbcsaTn6",
+  host: "164.92.196.198",
+  user: "nodejs",
+  password: "NodeJs%6363%JS",
   database: "sql11505843",
 });
 
